94f9b2a95a0e2326f5b4fbdf86ad12ba12d3c3c9
Antonio Thomas <antoniothomasjr@gmail.com>
2021-04-21 21:20:57 -0500

    first deploy

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,7 +4,7 @@ config()
 
 export async function connectDb() {
 	try {
-		const conn = await mongoose.connect(`${process.env.MONGO_URI}`, {
+		const conn = await mongoose.connect(`${process.env.MONGODB_URI}`, {
 			useCreateIndex: true,
 			useUnifiedTopology: true,
 			useNewUrlParser: true,
