a3610f7768b42016f76b47874a2abcf4daa11397
Antonio Thomas <antoniothomasjr@gmail.com>
2021-12-14 13:18:55 -0600

    npm update

--- server/src/types/ObjectIdScalar.ts
+++ server/src/types/ObjectIdScalar.ts
@@ -1,19 +1,20 @@
-import { GraphQLScalarType, Kind } from 'graphql'
-import { Types } from 'mongoose'
+import { GraphQLScalarType, Kind } from 'graphql';
+import { Types } from 'mongoose';
 
 export const ObjectIdScalar = new GraphQLScalarType({
 	name: 'ObjectId',
 	description: 'Mongo object id scalar type',
 	parseValue(value: string) {
-		return new Types.ObjectId(value) // value from the client input variables
+		return new Types.ObjectId(value); // value from the client input variables
 	},
 	serialize(value: Types.ObjectId) {
-		return value.toHexString() // value sent to the client
+		// @ts-ignore
+		return value.toHexString(); // value sent to the client
 	},
 	parseLiteral(ast) {
 		if (ast.kind === Kind.STRING) {
-			return new Types.ObjectId(ast.value) // value from the client query
+			return new Types.ObjectId(ast.value); // value from the client query
 		}
-		return null
+		return null;
 	},
-})
+});
